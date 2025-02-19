928a750d37090cd2036946a143227ac90a7c6818
luiza helena <luhealmo.moz@gmail.com>
2025-02-18 22:25:30 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+function showSidebar(event) {
+    if (event) {
+        event.preventDefault(); // Impede que o link redirecione a página para o topo
+    }
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
 }
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+
+function hideSidebar(event) {
+    if (event) {
+        if(event.target.tagName.toLowerCase() === 'a') {
+            // Não impede o comportamento padrão se o alvo for um link
+        } else {
+            event.preventDefault(); // Impede que o link redirecione a página para o topo
+        }
+    }
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
 }
+
+// Adiciona evento de clique a todos os links dentro do sidebar para fechar o menu
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        hideSidebar(event);
+    });
+});
