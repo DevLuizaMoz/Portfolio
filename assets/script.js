52e526d2805fdf1bf8ea588d64fe619d5e5a8ee3
luiza helena <luhealmo.moz@gmail.com>
2025-02-21 20:03:49 -0300

    atu

--- assets/script.js
+++ assets/script.js
@@ -64,4 +64,34 @@ observeLoader('loader-html-css', 'percentage-html-css', 90);
 observeLoader('loader-js', 'percentage-js', 85);
 observeLoader('loader-python', 'percentage-python', 80);
 
+let timeout; // Variável para armazenar o timeout
 
+document.addEventListener('scroll', function() {
+    const items = document.querySelectorAll('.timeline_item');
+    const progressBar = document.querySelector('.timeline_progress-bar');
+    const timelineProgress = document.querySelector('.timeline_progress');
+
+    // Obter a altura total da linha de progresso
+    const totalHeight = timelineProgress.offsetHeight;
+    const scrollPosition = window.scrollY; // Posição de rolagem atual
+
+    // Inicializa a altura atual da barra de progresso
+    let currentHeight = 0;
+
+    // Verifica a posição de cada item da timeline
+    items.forEach((item, index) => {
+        const itemPosition = item.getBoundingClientRect().top + window.scrollY; // Posição do item
+
+        // Se a posição de rolagem estiver acima do item, inicie o timeout
+        if (scrollPosition >= itemPosition) {
+            // Limpa o timeout anterior, se houver
+            clearTimeout(timeout);
+
+            // Inicia um novo timeout de 0.5s
+            timeout = setTimeout(() => {
+                currentHeight = ((index + 1) / items.length) * totalHeight; // Calcula a altura da barra
+                progressBar.style.height = `${currentHeight}px`; // Atualiza a altura da barra de progresso
+            }, 500); // 500ms de atraso
+        }
+    });
+});
