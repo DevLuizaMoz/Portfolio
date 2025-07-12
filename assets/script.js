2f685d0dadacebe2fab89d68ee8789cb7fb80e50
luiza helena <luhealmo.moz@gmail.com>
2025-07-11 23:27:29 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function showSidebar(event) {
     if (event) {
-        event.preventDefault(); // Impede que o link redirecione a página para o topo
+        event.preventDefault();
     }
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -9,16 +9,14 @@ function showSidebar(event) {
 function hideSidebar(event) {
     if (event) {
         if(event.target.tagName.toLowerCase() === 'a') {
-            // Não impede o comportamento padrão se o alvo for um link
         } else {
-            event.preventDefault(); // Impede que o link redirecione a página para o topo
+            event.preventDefault();
         }
     }
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
 
-// Adiciona evento de clique a todos os links dentro do sidebar para fechar o menu
 document.querySelectorAll('.sidebar a').forEach(link => {
     link.addEventListener('click', (event) => {
         hideSidebar(event);
@@ -29,18 +27,14 @@ function setLoaderPercentage(loaderId, percentageId, value) {
     const loaderCircle = document.getElementById(loaderId).querySelector('circle');
     const loaderPercentage = document.getElementById(percentageId);
     
-    // Limitar o valor entre 0 e 100
     value = Math.min(Math.max(value, 0), 100);
     
-    // Calcular o comprimento do traço
-    const circumference = 2 * Math.PI * 45; // 45 é o raio do círculo
+    const circumference = 2 * Math.PI * 45; 
     const offset = circumference - (value / 100) * circumference;
-    
-    // Atualizar o stroke-dashoffset do círculo
+   
     loaderCircle.style.strokeDasharray = `${circumference}`;
     loaderCircle.style.strokeDashoffset = `${offset}`;
     
-    // Atualizar o texto de porcentagem
     loaderPercentage.textContent = `${value}%`;
 }
 
